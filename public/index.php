649d6b16ac9a33e8134285bd9ef7e7bbd7c06b28
isanasan <yfcgpsebp@gmail.com>
2021-06-25 18:43:42 +0900

    rename info -> index

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+error_reporting(-1);
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+$psr7Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
+
+$creator = new \Nyholm\Psr7Server\ServerRequestCreator(
+    $psr7Factory,
+    $psr7Factory,
+    $psr7Factory,
+    $psr7Factory
+);
+
+$serverRequest = $creator->fromGlobals();
+
+dd($serverRequest->getUri());
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 php serve debug client
-> php -S 127.0.0.1:3939 public/info.php -t public
+> php -S 127.0.0.1:3939 public/index.php -t public
